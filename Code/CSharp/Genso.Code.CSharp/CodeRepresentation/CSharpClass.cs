cea6f409c4771514815e51764b849c8b620a0b78
Nils Feierabend <nevigskcuforez@gmail.com>
2021-02-05 23:59:07 +0100

    csharp class xml docs

--- Code/CSharp/Genso.Code.CSharp/CodeRepresentation/CSharpClass.cs
+++ Code/CSharp/Genso.Code.CSharp/CodeRepresentation/CSharpClass.cs
@@ -9,6 +9,11 @@ namespace Genso.Code.CSharp.CodeRepresentation
     /// Represents a CSharp class
     /// </summary>
     /// <param name="Name">Name of the class</param>
+    /// <param name="Namespace">Namespace the class is in</param>
+    /// <param name="Accessibility"><see cref="Microsoft.CodeAnalysis.Accessibility"/> of the class</param>
+    /// <param name="Usings">Usings the class uses</param>
+    /// <param name="Static">If the class is static</param>
+    /// <param name="Partial">If the class is partial</param>
 #pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public record CSharpClass(string Name, string? Namespace, Accessibility? Accessibility, ImmutableArray<string> Usings, bool Static, bool Partial) : Class(Name);
