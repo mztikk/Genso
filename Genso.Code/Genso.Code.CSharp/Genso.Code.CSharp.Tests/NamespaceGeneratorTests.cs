cf7a4ca50bf792d374d1f1b46805d87c5ae6e7a1
Nils Feierabend <nevigskcuforez@gmail.com>
2021-02-05 21:33:39 +0100

    added unit test projects and namespace generator tests

---
+++ Genso.Code/Genso.Code.CSharp/Genso.Code.CSharp.Tests/NamespaceGeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Genso.Code.CSharp.Generator;
+using Genso.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Genso.Code.CSharp.Tests
+{
+    [TestClass]
+    public class NamespaceGeneratorTests : BaseTest
+    {
+        [TestMethod]
+        public void StartsWithNamespace() => StartsWith(() => string.Empty, "namespace");
+
+        [DataTestMethod]
+        [DataRow("test")]
+        public void NamespaceNameTest(string name)
+        {
+            Contains(() => name, name);
+            StartsWith(() => name, $"namespace {name}");
+        }
+
+        [TestMethod]
+        public void NamespaceStructureTest() => Matches(() => string.Empty, new Regex("namespace .*\\{.*\\}", RegexOptions.Singleline));
+
+        [DataTestMethod]
+        [DataRow("test")]
+        public void NamespaceStructureWithNameTest(string name) => Matches(() => name, new Regex($"namespace {name}.*\\{{.*\\}}", RegexOptions.Singleline));
+
+        private static void StartsWith(Func<string> namespaceFactory, string substring) => StringAssert.StartsWith(Namespace(namespaceFactory), substring);
+        private static void Contains(Func<string> namespaceFactory, string substring) => StringAssert.Contains(Namespace(namespaceFactory), substring);
+        private static void Matches(Func<string> namespaceFactory, Regex pattern) => StringAssert.Matches(Namespace(namespaceFactory), pattern);
+
+        private static string Namespace(Func<string> namespaceFactory) => GetString((indentedWriter) => new NamespaceGenerator(namespaceFactory(), indentedWriter).Make());
+    }
+}
---
+++ Genso.Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Genso.IO;
+
+namespace Genso.Tests
+{
+    public class BaseTest
+    {
+        protected static Stream GetStream() => new MemoryStream();
+
+        protected static string GetString(Stream stream)
+        {
+            stream.Position = 0;
+            using var reader = new StreamReader(stream, leaveOpen: true);
+            return reader.ReadToEnd();
+        }
+
+        protected static string GetString(Action<IndentedStreamWriter> action)
+        {
+            using Stream stream = GetStream();
+            using IndentedStreamWriter indentedWriter = new IndentedStreamWriter(stream);
+            action(indentedWriter);
+            return GetString(stream);
+        }
+    }
+}
