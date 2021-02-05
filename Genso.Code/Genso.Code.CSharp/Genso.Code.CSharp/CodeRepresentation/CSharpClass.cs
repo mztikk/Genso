53e36d8153555fa6a18f22473d90c0c1ec17e6bc
Nils Feierabend <nevigskcuforez@gmail.com>
2021-02-05 20:12:15 +0100

    added Accessibility to CSharp Class

--- Genso.Code/Genso.Code.CSharp/Genso.Code.CSharp/CodeRepresentation/CSharpClass.cs
+++ Genso.Code/Genso.Code.CSharp/Genso.Code.CSharp/CodeRepresentation/CSharpClass.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using Genso.Code.CodeRepresentation;
+using Microsoft.CodeAnalysis;
 
 namespace Genso.Code.CSharp.CodeRepresentation
 {
@@ -10,7 +11,7 @@ namespace Genso.Code.CSharp.CodeRepresentation
     /// <param name="Name">Name of the class</param>
 #pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    public record CSharpClass(string Name, string? Namespace, ImmutableArray<string> Usings, bool @static, bool partial) : Class(Name);
+    public record CSharpClass(string Name, string? Namespace, Accessibility? Accessibility, ImmutableArray<string> Usings, bool Static, bool Partial) : Class(Name);
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning restore CS1572 // XML comment has a param tag, but there is no parameter by that name
 #pragma warning restore CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
