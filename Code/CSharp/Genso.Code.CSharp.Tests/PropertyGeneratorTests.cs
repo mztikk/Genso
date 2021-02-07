8e248fa32018fdc0775966cf26ffbfc1a65c7073
Nils Feierabend <nevigskcuforez@gmail.com>
2021-02-07 18:10:00 +0100

    implemented basic auto property generation with tests

---
+++ Code/CSharp/Genso.Code.CSharp.Tests/PropertyGeneratorTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Genso.Code.CSharp.CodeRepresentation;
+using Genso.Code.CSharp.Generator;
+using Genso.Tests;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Genso.Code.CSharp.Tests
+{
+    [TestClass]
+    public class PropertyGeneratorTests : BaseTest
+    {
+        [TestMethod]
+        public void EmptyGet() => Contains(GetAutoProperty, "get;");
+        [TestMethod]
+        public void EmptySet() => Contains(GetAutoProperty, "set;");
+        [TestMethod]
+        public void PrivateSet() => Contains(GetPrivateSetProperty, "private set");
+        [TestMethod]
+        public void AutoProperty()
+        {
+            Contains(GetAutoProperty, "get;");
+            Contains(GetAutoProperty, "set;");
+        }
+
+        [TestMethod]
+        public void Static() => Contains(GetStaticProperty, "static");
+
+        [TestMethod]
+        public void NotStatic() => DoesNotMatch(GetNonStaticProperty, new Regex(".*static.*"));
+
+        [DataTestMethod]
+        [DataRow("test")]
+        [DataRow("Property")]
+        [DataRow("äöü?ßá`àá")]
+        public void PropertyName(string name) => Contains(GetNameFactory(name), name);
+
+        [DataTestMethod]
+        [DataRow("string")]
+        [DataRow("int")]
+        [DataRow("float")]
+        public void PropertyType(string type) => Contains(GetTypeFactory(type), type);
+
+        [DataTestMethod]
+        [DataRow(Accessibility.Public)]
+        [DataRow(Accessibility.Private)]
+        [DataRow(Accessibility.Protected)]
+        [DataRow(Accessibility.Internal)]
+        [DataRow(Accessibility.ProtectedAndInternal)]
+        public void AccessibilityTests(Accessibility accessibility) => Contains(GetAccessibilityFactory(accessibility), accessibility.ToGeneratorString());
+
+        private static void EndsWithTrimmed(Func<CSharpProperty> propertyFactory, string substring) => StringAssert.EndsWith(Property(propertyFactory).Trim(), substring);
+        private static void Contains(Func<CSharpProperty> propertyFactory, string substring) => StringAssert.Contains(Property(propertyFactory), substring);
+        private static void DoesNotMatch(Func<CSharpProperty> propertyFactory, Regex pattern) => StringAssert.DoesNotMatch(Property(propertyFactory), pattern);
+
+        private static string Property(Func<CSharpProperty> propertyFactory) => GetString((indentedWriter) => new PropertyGenerator(propertyFactory(), indentedWriter).Make());
+
+        private static CSharpProperty GetPublicProperty() => new CSharpProperty(Accessibility.Public, false, "typename", "Propertyname", null, null, null, null, null);
+        private static CSharpProperty GetAutoProperty() => new CSharpProperty(Accessibility.Public, false, "typename", "Propertyname", null, null, null, null, null);
+        private static CSharpProperty GetPrivateSetProperty() => new CSharpProperty(Accessibility.Public, false, "typename", "Propertyname", null, null, Accessibility.Private, null, null);
+        private static CSharpProperty GetStaticProperty() => new CSharpProperty(Accessibility.Public, true, "typename", "Propertyname", null, null, null, null, null);
+        private static CSharpProperty GetNonStaticProperty() => new CSharpProperty(Accessibility.Public, false, "typename", "Propertyname", null, null, null, null, null);
+        private static Func<CSharpProperty> GetNameFactory(string name) => () => new CSharpProperty(Accessibility.Public, false, "typename", name, null, null, null, null, null);
+        private static Func<CSharpProperty> GetTypeFactory(string type) => () => new CSharpProperty(Accessibility.Public, false, type, "Propertyname", null, null, null, null, null);
+        private static Func<CSharpProperty> GetAccessibilityFactory(Accessibility accessibility) => () => new CSharpProperty(accessibility, false, "typename", "Propertyname", null, null, null, null, null);
+    }
+}
---
+++ Code/CSharp/Genso.Code.CSharp/Generator/PropertyGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Genso.Code.CSharp.CodeRepresentation;
+using Genso.Generator;
+using Genso.IO;
+
+namespace Genso.Code.CSharp.Generator
+{
+    /// <summary>
+    /// Class to generate a C# Property
+    /// </summary>
+    public class PropertyGenerator : BaseGenerator
+    {
+        private readonly CSharpProperty _property;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyGenerator"/> class for the specified <see cref="IndentedStreamWriter"/>
+        /// </summary>
+        /// <param name="property">Property to generate</param>
+        /// <param name="writer"><see cref="IndentedStreamWriter"/> used to write to</param>
+        public PropertyGenerator(CSharpProperty property, IndentedStreamWriter writer) : base(writer) => _property = property;
+
+        /// <summary>
+        /// Begins writing the namespace
+        /// </summary>
+        /// <returns><see langword="true"/> if it wrote anything</returns>
+        public override bool Begin()
+        {
+            Write(string.Join(" ", GetPropertyDescription()));
+
+            Write(" { ");
+
+            if (_property.GetterAccessibility.HasValue)
+            {
+                Write(_property.GetterAccessibility.Value.ToGeneratorString());
+                Write(" ");
+            }
+            Write("get;");
+
+            Write(" ");
+
+            if (_property.SetterAccessibility.HasValue)
+            {
+                Write(_property.SetterAccessibility.Value.ToGeneratorString());
+                Write(" ");
+            }
+            Write("set;");
+
+            Write(" } ");
+            WriteLine();
+
+            return true;
+        }
+
+        private IEnumerable<string> GetPropertyDescription()
+        {
+            yield return _property.Accessibility.ToGeneratorString();
+
+            if (_property.Static)
+            {
+                yield return "static";
+            }
+
+            yield return _property.Type;
+            yield return _property.Name;
+        }
+
+        /// <summary>
+        /// Does not write anything
+        /// </summary>
+        /// <returns>Always <see langword="false"/></returns>
+        public override bool End() => false;
+    }
+}
